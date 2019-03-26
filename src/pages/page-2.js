594a3632d3d048376f851d6d7a5f6c399ff9ba40
Kimber Gee <kimber.gee@gmail.com>
2019-03-26 14:31:17 -0400

    go thru css and animations things

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const SecondPage = () => (
   <div>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Hi from the second page</h1>
+        <p>Welcome to page 2</p>
+        <Link to="/">Go back to the homepage</Link>
+      </div>
+    </div>
   </div>
 )
 
